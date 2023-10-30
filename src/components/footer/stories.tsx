d846cc596a6f0444c85d76ee558abc11ad70d14a
HoSiuFu <cnjpkrtw@gmail.com>
2023-10-30 20:44:44 +0000

    feat: The components are all done but the paddings need to be fixed in some instances

---
+++ src/components/footer/stories.tsx
@@ -0,0 +1,11 @@
+import Footer from '.'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta = {
+    title: 'Atoms/Footer',
+    component: Footer,
+}
+
+export default meta
+
+export const Default: StoryObj = {}
