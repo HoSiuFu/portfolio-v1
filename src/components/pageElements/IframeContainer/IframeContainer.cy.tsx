2d3137bf99953b5ad6ebd344ac0a1cb45de3b361
HoSiuFu <cnjpkrtw@gmail.com>
2023-11-16 17:29:36 +0000

    fix: Fixing tests

--- src/components/pageElements/IframeContainer/IframeContainer.cy.tsx
+++ src/components/pageElements/IframeContainer/IframeContainer.cy.tsx
@@ -6,8 +6,12 @@ describe('<IframeContainer />', () => {
     it('Renders', () => {
         cy.mount(
             <IframeContainer
-                src='https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtWISqbVVkIsSxS5jaQnk3s%2FPortofolio-V1%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3Dw1NK5QTpQgKqMxWp'
-                title='test'
+                iframeOptions={[
+                    {
+                        src: '/',
+                        title: 'test',
+                    },
+                ]}
             />
         )
     })
